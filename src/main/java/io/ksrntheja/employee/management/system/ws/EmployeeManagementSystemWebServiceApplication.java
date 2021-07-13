df966700351e4ef99a17dbbeb992b0c10a4e4dce
KSRNTheja <srntkolla@gmail.com>
2021-07-13 13:16:34 +0530

    00NewProject

---
+++ src/main/java/io/ksrntheja/employee/management/system/ws/EmployeeManagementSystemWebServiceApplication.java
@@ -0,0 +1,13 @@
+package io.ksrntheja.employee.management.system.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementSystemWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ksrntheja/employee/management/system/ws/EmployeeManagementSystemWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ksrntheja.employee.management.system.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
